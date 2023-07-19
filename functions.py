90da7bd2ee32678923d38af1c3c4a6ec0cbba370
Илья Боярский <boyailya@gmail.com>
2023-07-19 15:53:26 +0300

    show and create projects

--- functions.py
+++ functions.py
@@ -41,18 +41,34 @@ def select(query):
         connection.close()
 
 
-def create_projects(args: dict):
+def create_projects(args: dict) -> tuple:
     args['date'] = datetime.today().strftime(('%Y-%m-%d'))
-    args['proj_owner'] = 'Ilusha Tester'
 
+    args['is_favorites'] = 0 if not args['is_favorites'] else 1
     values = tuple(args.values())
-    query_insert = f'''INSERT INTO `Project` 
+    query_insert = f'''
+    INSERT INTO `Project` 
     {tuple(args)} VALUES '''
 
     query_insert = query_insert.replace("'", "") + str(values)    
     insert(query_insert)
 
     return {'messege': "ok"}, 200
+
+
+def project_list(user='Ilusha'):
+
+    query_select = f'SELECT id, name FROM Project WHERE owner = {user}'
+    select_proj = select(query_select)
+    send_list = []
+    table_keys = ['id', 'name']
+    for i in select_proj:
+        dict_to_append = dict(zip(table_keys, i))
+        send_list.append(dict_to_append)
+
+    send_dict = {'project_list':send_list}
+
+    return send_dict, 200
 
 
 def create_task(args: dict):
@@ -99,25 +115,32 @@ def user(args: dict):
     return {'messege': "ok"}, 200
 
 
-def get_projects():
+def get_projects() -> tuple:
     query_select = '''
-    SELECT Project.project_name, Project.is_favorites, 
-    COUNT(Task.project_name) 
-    FROM `Project` JOIN Task ON Project.project_name = Task.project_name 
-    WHERE Task.project_name = 'testing' 
-    GROUP BY 
-    Project.project_name, Project.is_favorites
+    SELECT Project.name, Project.is_favorites, COUNT(Task.id) 
+    FROM `Project` 
+    LEFT JOIN Task 
+    ON 
+    Project.id = Task.project_id 
+    GROUP BY
+    Project.name, Project.is_favorites
     '''
     table_keys = ['project_name', 'is_favorites', 'task_count']
 
     data_to_show = select(query_select)
     send_list = []
     for i in data_to_show:
+        i = list(i)
+        if i[1] == 0:
+            i[1] = False
+        else:
+            i[1] = True
         dict_to_append = dict(zip(table_keys, i))
         send_list.append(dict_to_append)
 
     projects = {'projects':send_list}
-    return json.dumps(projects), 200
+    return projects, 200
+
 
 {
     "projects": 
@@ -129,3 +152,5 @@ def get_projects():
         }
     ]
 }
+
+# create_task({'name':'idea', 'description':'empty_field', 'project_id':7})
--- main.py
+++ main.py
@@ -1,38 +1,56 @@
-# from flask import Flask, Response, json, send_file
-# from flask_restful import Api, reqparse, Resource
-# from flask.wrappers import Request
-# from werkzeug.datastructures import FileStorage
-# from datetime import datetime
-
-# class AnyJsonRequest(Request):
-#     def on_json_loading_failed(self, e):
-#         if e is not None:
-#             return super().on_json_loading_failed(e)
-
-
-# class _Resource(Resource):
-#     parser = reqparse.RequestParser(trim=True)
-
-#     def return_json(self, body, status):
-#         return Response(json.dumps(body, ensure_ascii=False), mimetype='application/json', status=status)
-
-#     def return_status(self, status):
-#         return Response(status=status)
-
-
-# app = Flask(__name__)
-# api = Api(app, prefix='')
-# app.request_class = AnyJsonRequest
-
-from flask import Flask
+from flask import Flask, Response, json
+from flask_restful import Api, reqparse, Resource
+from flask.wrappers import Request
 from functions  import *
 from config import API
 
-app = Flask(__name__)
+class AnyJsonRequest(Request):
+    def on_json_loading_failed(self, e):
+        if e is not None:
+            return super().on_json_loading_failed(e)
 
-@app.route('/')
-def get_proj():
-    return get_projects()
+
+class _Resource(Resource):
+    parser = reqparse.RequestParser(trim=True)
+
+    def return_json(self, body, status):
+        return Response(json.dumps(body, ensure_ascii=False), mimetype='application/json', status=status)
+
+    def return_status(self, status):
+        return Response(status=status)
+
+
+app = Flask(__name__)
+api = Api(app, prefix='')
+app.request_class = AnyJsonRequest
+
+class ParseBool():
+    def __new__(cls, x: str) -> bool:
+        if x.lower() == 'true':
+            return True
+        return False
+
+
+
+class Project(_Resource):
+    """Загрузить данные из файла и отправка их в верном формате в таблицу (на фронт)"""
+    parser = reqparse.RequestParser(trim=True)
+    parser.add_argument('name', type=str)
+    parser.add_argument('is_favorites', type=ParseBool)
+
+
+    def get(self):
+        body, status = get_projects()
+        return self.return_json(body, status)
+    
+
+    def post(self):
+        args: dict = self.parser.parse_args()
+        return self.return_json(*create_projects(args))
+
+
+api.add_resource(Project, '/projects')
+
 
 
 if __name__ == '__main__':

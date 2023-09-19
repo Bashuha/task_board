8f52afd7c42f333620b41bd0c4cf6399da3a432f
Илья Боярский <boyailya@gmail.com>
2023-09-19 10:46:48 +0300

    delete extra files

--- FastApi_board/database/config.py
+++
@@ -1,6 +0,0 @@
-from configparser import ConfigParser
-config_with_global = ConfigParser()
-config_with_global.read('settings.ini')
-
-API = config_with_global['API']
-MYSQL = config_with_global['MYSQL']

4bdff407d724b52c777c60f39d093b759c99b069
Илья Боярский <boyailya@gmail.com>
2024-08-28 16:28:23 +0300

    delete bash scripts

--- start_api.sh
+++
@@ -1,6 +0,0 @@
-DIRNAME=`dirname $0`
-cd $DIRNAME
-source venv/bin/activate
-PYTHON_PATH=`which python`
-PID=`realpath api.pid`
-start-stop-daemon -S -b -x $PYTHON_PATH -d ./ -m -v -p $PID -- ./main.py
--- stop_api.sh
+++
@@ -1,4 +0,0 @@
-DIRNAME=`dirname $0`
-cd $DIRNAME
-PID=`realpath api.pid`
-start-stop-daemon -K -p $PID --remove-pidfile
